57f022e283dc247b9f0ddaeb1f2942f5c498152e
sszz01 <resios027@gmail.com>
2025-08-01 14:55:54 -0700

    Initial commit

--- src/main.py
+++ src/main.py
@@ -1,3 +1,32 @@
-bag_of_words = list(i for i in range(0,10))
-obj =
-print(bag_of_words)
+import cv2
+import time
+
+STREAM_SRC = "https://www.bloomberg.com/media-manifest/streams/aus.m3u8" # sample stream
+
+def main():
+    start_time = time.time()
+    cap = cv2.VideoCapture(STREAM_SRC)
+    if not cap.isOpened():
+        print(f"cannot open video source: {STREAM_SRC}")
+        return
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    wt = 1 / fps if fps > 0 or fps != fps else exit(1)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        cv2.imshow("Frame", frame)
+        if cv2.waitKey(33) & 0xFF == ord('q'):
+            break
+        dt = time.time() - start_time
+        if wt - dt > 0:
+            time.sleep(wt - dt)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
